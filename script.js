958273705b01be77b21d32c3a87109d3d1eb81d5
dylanmc1ntee <dylangmcintee@gmail.com>
2024-03-12 22:11:49 -0400

    attempt add and remove function

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 var now = new Date();
 var datetime = now.toDateString();
 document.getElementById("date").innerHTML = datetime;
+
+const inputBox = document.getElementById("weight");
+const listContainer = document.getElementById("log-container");
+
+function addToDo()
+{
+    if(inputBox.value === '')
+    {
+        alert("Enter weight.");
+    }
+    else
+    {
+        let li = document.createElement("li");
+        // li.getElementById("curDate").innerHTML = datetime;
+        // li.getElementById("curWeight").innerHTML = inputBox.value;
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e)
+{
+    if(e.target.tagName == "LI")
+    {
+        e.target.remove();
+        saveData();
+    }
+}, false);
+
+function saveData()
+{
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function displaySaved()
+{
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+displaySaved();
